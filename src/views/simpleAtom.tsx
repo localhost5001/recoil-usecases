15574427db966b30c020ea51060bc1cc9b1f4548
Leo Kim <e_leokim@yahoo.com>
2022-06-23 10:25:53 -0700

    Configure typescript

--- src/views/simpleAtom.tsx
+++ src/views/simpleAtom.tsx
@@ -1,5 +1,5 @@
 import { useRecoilValue } from 'recoil'
-import { userState } from '../state/simpleAtom'
+import { userState } from 'state/simpleAtom'
 
 const SimpleAtom = () => {
     const user = useRecoilValue(userState)
